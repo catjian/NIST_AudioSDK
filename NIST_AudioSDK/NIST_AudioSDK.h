d7e8d15f966807adf633a46c96297612e6b0e68f
zhangjian <zhangjian@zhangjiantekimac-mini.local>
2015-11-30 14:26:51 +0800

    Initial Commit

---
+++ NIST_AudioSDK/NIST_AudioSDK.h
@@ -0,0 +1,13 @@
+//
+//  NIST_AudioSDK.h
+//  NIST_AudioSDK
+//
+//  Created by zhangjian on 15/11/30.
+//  Copyright © 2015年 zhangjian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NIST_AudioSDK : NSObject
+
+@end
---
+++ NIST_AudioSDK/NIST_AudioSDK.m
@@ -0,0 +1,13 @@
+//
+//  NIST_AudioSDK.m
+//  NIST_AudioSDK
+//
+//  Created by zhangjian on 15/11/30.
+//  Copyright © 2015年 zhangjian. All rights reserved.
+//
+
+#import "NIST_AudioSDK.h"
+
+@implementation NIST_AudioSDK
+
+@end
